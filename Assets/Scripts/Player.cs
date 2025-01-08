2fde79a0282fda9c3b2e571a8ef051bec94d86f2
funkykong1 <vilho.valkeala@gmail.com>
2025-01-08 13:41:09 +0200

    tsg

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 using System;
 using UnityEditor.Build.Content;
 using Unity.Mathematics;
-
+  
 
 public class Player : MonoBehaviour
 {
-
+ 
     private  Vector2 mousePosition;
     private Camera mainCamera;
     //private GameManager gameManager;
 
-    public float rotateSpeed;
+    float horizontalInput;
+    float verticalInput;
+    public float shipSpeed;
 
     void Awake()
     {
@@ -21,21 +23,29 @@ public class Player : MonoBehaviour
         //gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
     }
-
+     
     // Update is called once per frame
     void Update()
     {
         RotateToMouse();
+        Move();
     }
+
 
     void RotateToMouse()
     {
-        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    Vector2 targetDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+    transform.rotation = Quaternion.FromToRotation(Vector3.up, targetDirection);
+    }
+    
+    void Move()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * shipSpeed);
 
-        Vector2 targetDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-
- 
-
-
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * shipSpeed);
+        
     }
 }
